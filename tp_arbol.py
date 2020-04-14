acce5c314eb16d0f233c35c77daf3128d3079496
Franco Valledor <francovalledor@gmail.com>
2020-04-14 16:59:22 -0300

    EJ1.a EJ1.b

--- tp_arbol.py
+++ tp_arbol.py
@@ -1,9 +1,27 @@
 from TDA_arbol import ArbolBinario
 from random import randint
 
+
+#EJ1
 arbol = ArbolBinario()
-
-for i in range(0,100):
-    arbol.insertar(randint(0,1000))
-
+for i in range(0,1000):
+    arbol.insertar(randint(0,10000))
+#EJ1.a
+print("INORDEN")
 arbol.mostrar_inorden()
+print("-"*50)
+
+print("PREORDEN")
+arbol.mostrar_preorden()
+print("-"*50)
+
+print("POSTORDEN")
+arbol.mostrar_postorden()
+print("-"*50)
+
+
+#EJ1.b
+numero_azar = randint(0,10000)
+print("-"*50)
+print(f"el numero {numero_azar} ¿se encuentra en el arbol?")
+print("Sí" if arbol.buscar(numero_azar) else "No")
